73f3f20f534af9f45207a448e4ef67db81bde237
dipityg <seryguna@gmail.com>
2021-11-06 21:26:33 -0400

    added mel's changes

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-import Papa from "https://cdn.skypack.dev/papaparse@5.3.0";
-// load();
+//import Papa from "https://cdn.skypack.dev/papaparse@5.3.0";
+load();
 // htmlIntro();
 // htmlGame();
+
+//makeChart();
 
 arrayparse(rawdata);
